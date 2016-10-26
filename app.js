406ef109c652b6eb59e901cec01fe606f0f5576c
Jeff Carver <maxapps@gmail.com>
2016-10-26 19:12:17 -0400

    Using cordova-plugin-media

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
 tabris.create('Page', {
-        title: 'App',
-        topLevel: true
+  title: 'App',
+  topLevel: true
 }).append(
-        tabris.create('TextView', {
-                text: 'cordova-tabris-nativeaudio'
-        })
+  tabris.create('TextView', {
+    text: 'cordova-plugin-media'
+  })
 ).open();
 
-window.plugins.NativeAudio.preloadComplex('mysound',
-        tabris.app.getResourceLocation('demo.mp3'),
-        1, // volume
-        1, // voices
-        0, // delay
-// window.plugins.NativeAudio.preloadSimple('mysound',
-//         tabris.app.getResourceLocation('demo.mp3'),
-        function() {
-                console.info('Preload success: ', arguments);
-                window.plugins.NativeAudio.play('mysound', function(msg) {
-                        console.info('Play success: ', arguments)
-                },
-                function(msg) {
-                        console.warn('Play error: ', arguments);
-                },
-                function(msg) {
-                        console.info('Play complete: ', arguments);
-                });
-        },
-        function(msg) {
-                console.warn('Preload error: ', arguments);
-        }
-);
 
+if (window.plugins.Media) {
+  var media = new Media(tabris.app.getResourceLocation('demo.mp3'), 
+    function() {
+      console.log("playAudio():Audio Success");
+    },
+    function(err) {
+      console.log("playAudio():Audio Error: " + err);
+    }
+  );
 
-/*
-The old format of this exec call has been removed (depreciated since 2.1).
-Change to: cordova.exec(null, null, "http://192.168.1.3:8080/demo", "mp3",[null,"NativeAudio","preloadComplex",["mysound","http://192.168.1.3:8080/demo.mp3",1,1,0]]);
-*/
+  media.play();
+} else {
+  console.log('Media plugin not included in build');
+}
